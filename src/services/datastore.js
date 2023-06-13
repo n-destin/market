53a78b8b6dc486d114cc41d23938f2b9890792d0
Destin Niyomufasha <destin.niyomufasha.26@dartmouth.edu>
2023-06-13 14:59:58 -0400

    changes

--- src/services/datastore.js
+++ src/services/datastore.js
@@ -4,23 +4,22 @@ import { data } from 'jquery';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBDvOBf2m2YQfyJ5rOC3mIQ4L2kmJdm3OY",
-    authDomain: "notesapp-c668e.firebaseapp.com",
-    databaseURL: "https://notesapp-c668e-default-rtdb.firebaseio.com",
-    projectId: "notesapp-c668e",
-    storageBucket: "notesapp-c668e.appspot.com",
-    messagingSenderId: "499481922320",
-    appId: "1:499481922320:web:ed22fc465ffd284c4db395",
-    measurementId: "G-4LHZL9HJZ1"
-  };
-
+  apiKey: "AIzaSyBDvOBf2m2YQfyJ5rOC3mIQ4L2kmJdm3OY",
+  authDomain: "notesapp-c668e.firebaseapp.com",
+  databaseURL: "https://notesapp-c668e-default-rtdb.firebaseio.com",
+  projectId: "notesapp-c668e",
+  storageBucket: "notesapp-c668e.appspot.com",
+  messagingSenderId: "499481922320",
+  appId: "1:499481922320:web:ed22fc465ffd284c4db395",
+  measurementId: "G-4LHZL9HJZ1"
+};
 
   firebase.initializeApp(firebaseConfig)
   const database = firebase.database();
   
   export function onValueChange(callback) {
-      database.ref("notes").on('value', (snapshort)=>{
-      callback(snapshort.val())
+      database.ref('notes').on('value', (snapshort)=>{
+      callback(snapshort.val());
     })
   }
 
